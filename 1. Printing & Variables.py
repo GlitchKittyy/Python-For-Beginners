2642686afb8c68227748bd34122d3c4736dda29e
Yanick <112824345+GlitchKittyy@users.noreply.github.com>
2023-12-27 16:17:09 +0100

    Create 1. Printing & Variables.py

---
+++ 1. Printing & Variables.py
@@ -0,0 +1,82 @@
+# Python For Beginners
+#
+# 1. Print & Variables
+
+print("Hello World!")
+# Outcome: Hello World!
+
+# You use the print function when you want to print
+# things to the console
+
+# Example:
+
+print("Hello, I like food!")
+# Outcome: 
+
+# A variable holds a value that can be changed
+# To make a variable just give it a name
+
+# Example: Hello, I like food!
+
+name = "John"
+
+# Here I've created a variable with a value of "John"
+# In this case the name variable holds a value that is a string
+# A string is used for text
+# When you make a variable you cant use spaces for the name
+# you can use underscores and numbers
+
+# To use variables in a print function add it in the print function
+
+# Example:
+
+print(name)
+# Outcome: John
+
+# To add additional text use the '+' to add text or variables in
+# the print function
+
+# Example:
+
+print("My name is" + name)
+# Outcome: My name isJohn
+
+# In this case you need to add a space in the string that holds
+# the first bit of text
+# Another way is to use ',' this will automaticly add a space
+
+# Example:
+
+print("My name is", name)
+# Outcome: My name is John
+
+# For numbers the variable is either a int or a float
+# An int holds full numbers like 1 2 3
+# Whilst a float holds numbers with a decimal like 1.0 2.4 3.1
+
+# Example:
+
+number1 = 1
+
+# In this case number1 is a int that holds a value of 1
+# For a float you just need to add a . with a number after it
+
+# Example:
+
+number2 = 2.0
+
+# You can change the value of the variables
+
+# Example
+
+number1 = 2
+
+# We changed number1 from 1 to 2
+
+# Try it your self!
+#
+# ---
+#
+# Make a variable called name and make it store your own name
+# and print the name variable
+
